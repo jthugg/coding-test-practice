baf1eb542120e35bede2d05fb25999cb41bbbf02
Hi. I'm Neo <116662930+jthugg@users.noreply.github.com>
2022-12-28 01:40:21 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.33 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        if (numbers[0] * numbers[1] > numbers[numbers.length - 2] * numbers[numbers.length - 1]) {
+            return numbers[0] * numbers[1];
+        }
+        return numbers[numbers.length - 2] * numbers[numbers.length - 1];
+    }
+}
