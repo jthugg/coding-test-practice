3faf98c0070535ed554a55c8218abace861bfe67
Hi. I'm Neo <116662930+jthugg@users.noreply.github.com>
2022-12-28 18:41:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+    public String solution(String[] id_pw, String[][] db) {
+        for (String[] data : db) {
+            if (data[0].equals(id_pw[0])) {
+                if(data[1].equals(id_pw[1])) {
+                    return "login";
+                }
+                return "wrong pw";
+            }
+        }
+        return "fail";
+    }
+}
