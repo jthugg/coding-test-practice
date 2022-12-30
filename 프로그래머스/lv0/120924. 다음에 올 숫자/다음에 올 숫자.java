358771a51b7db3529f7092964971bb0a5359ad93
Hi. I'm Neo <116662930+jthugg@users.noreply.github.com>
2022-12-31 03:38:26 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] common) {
+        if (common[0] - common[1] == common[1] - common[2]) {
+            return common[common.length - 1] - (common[0] - common[1]);
+        }
+        return common[common.length - 1] * (common[1] / common[0]);
+    }
+}
