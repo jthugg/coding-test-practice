e60241283b2c51703323d192a5704f053cacea36
Hi. I'm Neo <116662930+jthugg@users.noreply.github.com>
2023-01-07 23:59:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 16.36 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return new int[] {-1};
+        }
+        return Arrays.stream(arr)
+                .filter(i -> i != Arrays.stream(arr).min().getAsInt())
+                .toArray();
+    }
+}
