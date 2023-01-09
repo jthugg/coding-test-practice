9242f0326e67789ade289ab2d342de3b7b3b4849
Hi. I'm Neo <116662930+jthugg@users.noreply.github.com>
2023-01-10 03:45:46 +0900

    [level 1] Title: [1차] 비밀지도, Time: 10.09 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        return IntStream.range(0, n).mapToObj(i -> String.format("%" + n + "s", Integer.toBinaryString(arr1[i] | arr2[i]).replaceAll("1", "#").replaceAll("0", " "))).toArray(String[]::new);
+    }
+}
