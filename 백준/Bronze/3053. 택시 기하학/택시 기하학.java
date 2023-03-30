4033175b1b6f73799ba24673e2c9f5286cd2bd33
Neo <116662930+jthugg@users.noreply.github.com>
2023-03-30 15:21:03 +0900

    [Bronze III] Title: 택시 기하학, Time: 216 ms, Memory: 17972 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double radius = new Scanner(System.in).nextDouble();
+        System.out.println(radius * radius * 3.14159265358979);
+        System.out.println(radius * radius * 2);
+    }
+}
