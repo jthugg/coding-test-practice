2e84b6807445bd5d2c1c1b2ff6ca1a68f94c2955
Neo <116662930+jthugg@users.noreply.github.com>
2023-06-06 15:56:17 +0900

    [Bronze V] Title: A+B - 3, Time: 228 ms, Memory: 17976 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        for (int i = 0; i < count; i++) {
+            System.out.println(scanner.nextInt() + scanner.nextInt());
+        }
+    }
+}
