a4fbc5dbe364fd80d19af064c1a6cda3248788f1
Neo <116662930+jthugg@users.noreply.github.com>
2023-06-25 20:08:47 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,12 @@
+select
+    ANIMAL_INS.NAME,
+    ANIMAL_INS.DATETIME
+from
+    ANIMAL_INS left join ANIMAL_OUTS
+        ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID
+where
+    ANIMAL_OUTS.ANIMAL_ID is null
+order by
+    ANIMAL_INS.DATETIME
+limit
+    3
