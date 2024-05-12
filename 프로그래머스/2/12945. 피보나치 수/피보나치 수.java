9b07c22cd93cbd1c5617a36cc817bbc17cf8e957
Neo <116662930+jthugg@users.noreply.github.com>
2024-05-13 02:51:25 +0900

    [level 2] Title: 피보나치 수, Time: 6.55 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,15 +1,39 @@
+// class Solution {
+
+//     public int solution(int n) {
+//         return getAnswer(0, 1, 1, n, 1);
+//     }
+    
+//     int getAnswer(int f0, int f1, int f2, int limit, int count) {
+//         if (count < limit) {
+//             count++;
+//             f2 = (f0 + f1) % 1234567;
+//             f0 = f1;
+//             f1 = f2;
+//             return getAnswer(f0, f1, f2, limit, count);
+//         }
+//         return f2;
+//     }
+
+// }
+
 class Solution {
 
     public int solution(int n) {
-        int f0 = 0;
-        int f1 = 1;
-        int f2 = 1;
-        for (int i = 2; i <= n; i++) {
-            f2 = (f0 + f1) % 1234567;
-            f0 = f1;
-            f1 = f2;
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 1;
+        return getValue(n, arr);
+    }
+    
+    int getValue(int n, int[] arr) {
+        if (n < 3) {
+            return arr[n];
         }
-        return f2;
+        if (arr[n] == 0) {
+            arr[n] = (getValue(n - 2, arr) + getValue(n - 1, arr)) % 1234567;
+        }
+        return arr[n];
     }
 
 }
